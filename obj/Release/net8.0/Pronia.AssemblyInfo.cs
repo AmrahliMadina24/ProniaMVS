a6f6d55d48985d5c127354cbb28aecd14e83ec1e
AmrahliMadina24 <madinava-mpa201@code.edu.az>
2025-12-23 23:10:40 -0800

    Update Function Added

--- obj/Release/net8.0/Pronia.AssemblyInfo.cs
+++ obj/Release/net8.0/Pronia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b6bd2c649d8a4065c17ca44d15551ac35bb117")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pronia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
